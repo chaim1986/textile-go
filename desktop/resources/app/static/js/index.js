89c9005d58b4bf5ccfa88c154d81f10639b8267d
andrew <andrewxhill@gmail.com>
2018-04-06 21:00:23 -0700

    desktop app is working. generates qr code for pairing, but not currently writing anything useful into it.

--- desktop/resources/app/static/js/index.js
+++ desktop/resources/app/static/js/index.js
@@ -1,69 +1,87 @@
 let index = {
-    // about: function(html) {
-    //     let c = document.createElement("div");
-    //     c.innerHTML = html;
-    //     asticode.modaler.setContent(c);
-    //     asticode.modaler.show();
-    // },
-    // addFolder(name, path) {
-    //     let div = document.createElement("div");
-    //     div.className = "dir";
-    //     div.onclick = function() { index.explore(path) };
-    //     div.innerHTML = `<i class="fa fa-folder"></i><span>` + name + `</span>`;
-    //     document.getElementById("dirs").appendChild(div)
-    // },
-    init: function() {
-        // Init
-        asticode.loader.init();
-        asticode.modaler.init();
-        asticode.notifier.init();
+  init: function() {
+    // Init
+    asticode.loader.init();
+    asticode.modaler.init();
+    asticode.notifier.init();
 
-        // Wait for astilectron to be ready
-        document.addEventListener('astilectron-ready', function() {
-            // Listen
-            index.listen();
+    // Wait for astilectron to be ready
+    document.addEventListener('astilectron-ready', function() {
+      // Listen
+      index.listen();
 
-            document.getElementById("path").innerHTML = "derp";
-
-            // just to see the peerid in the interface
-            asticode.notifier.info("derp")
-            astilectron.sendMessage({name: "ipfs.peerId", payload: "get"}, function(message) {
-                asticode.notifier.info(message)
-
-                // Get a thumbnail (i know it exists)
-                astilectron.sendMessage({name: "ipfs.getPath", payload: "QmR8mGCutBWDPBc9zdpPZPoRYRAJS7BMZhJtqHeFtJp2ma/thumb.JPG"}, function(message) {
-                    asticode.notifier.info(message)
-                    // Check error
-                    if (message.name === "error") {
-                        asticode.notifier.error(message.payload);
-                        return
-                    }
-                    document.getElementById("test").src = "data:image/png;base64, " + message.payload;
-                })
-
-
-                // Check error
-                if (message.name === "error") {
-                    asticode.notifier.error(message.payload);
-                    return
-                }
-                document.getElementById("message").innerHTML = message.payload;
-            })
-
+      let pairNewDevice = function () {
+        // Get a thumbnail (i know it exists)
+        astilectron.sendMessage({name: "peer.qr", payload: ""}, function (message) {
+          // Check error
+          console.log(message)
+          if (message.name === "error") {
+            asticode.notifier.error("Error");
+            return
+          }
+          let qrCode = document.querySelector('.peerQRCode');
+          qrCode.setAttribute('src', "data:image/png;base64, " + message.payload + "")
+          let modal = document.querySelector('.modal');
+          modal.classList.toggle('modal-open');
         })
-    },
-    listen: function() {
-        astilectron.onMessage(function(message) {
-            console.log(message.name)
-            switch (message.name) {
-                case "about":
-                    // index.about(message.payload);
-                    return {payload: "payload"};
-                    break;
-                case "check.out.menu":
-                    asticode.notifier.info(message.payload);
-                    break;
-            }
-        });
-    }
+      }
+      setupQRModal(pairNewDevice)
+    })
+  },
+  listen: function() {
+    astilectron.onMessage(function(message) {
+      switch (message.name) {
+        case "new.log":
+          let lines = document.getElementById("log").innerText;
+          document.getElementById("log").innerText = message.payload + "<br/>" + lines
+          break;
+        case "new.image":
+          addNewGalleryImage("https://gateway.ipfs.io/ipfs/QmR8mGCutBWDPBc9zdpPZPoRYRAJS7BMZhJtqHeFtJp2ma/thumb.jpg")
+          break;
+      }
+    });
+  },
 };
+
+function setupQRModal(pairMethod) {
+  let modal = document.querySelector('.modal');
+  let closeButtons = document.querySelectorAll('.close-modal');
+  // set open modal behaviour
+  document.getElementById('pairNewDevice').addEventListener('click', pairMethod);
+  // set close modal behaviour
+  for (i = 0; i < closeButtons.length; ++i) {
+    closeButtons[i].addEventListener('click', function() {
+      modal.classList.toggle('modal-open');
+    });
+  }
+  // close modal if clicked outside content area
+  document.querySelector('.modal-inner').addEventListener('click', function() {
+    modal.classList.toggle('modal-open');
+  });
+  // prevent modal inner from closing parent when clicked
+  document.querySelector('.modal-content').addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
+}
+
+function addNewGalleryImage(src) {
+  let entry = document.createElement('div');
+  entry.className = 'two columns entry';
+  let gallery = document.getElementById('gallery');
+  gallery.insertBefore(entry, gallery.firstChild);
+
+  let thumb = document.createElement('div');
+  thumb.className = 'row thumb';
+  entry.appendChild(thumb);
+
+  console.log(src)
+  let img = document.createElement("img");
+  img.className = 'u-max-full-width';
+  thumb.appendChild(img);
+  img.setAttribute('src', src);
+
+  let label = document.createElement('div');
+  label.className = 'row label u-max-full-width';
+  label.innerText = "Yesterday"
+  entry.appendChild(label);
+}
